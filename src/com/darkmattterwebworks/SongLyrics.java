0efeeeb2b37004653cc393376ce1249201bc8643
Cody Chase <cchase1991@gmail.com>
2025-02-14 15:16:01 -0500

    Assignment01 Complete

---
+++ src/com/darkmattterwebworks/SongLyrics.java
@@ -0,0 +1,19 @@
+package com.darkmattterwebworks;
+
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		/*
+		 * Print 4 lines from my favorite song.
+		 * The following is 4 lines from the song It's Going Down by Connor Price
+		 */
+		
+		System.out.println("Now I can moonwalk in two socks and groove all week");
+		System.out.println("But I can't seem to two-step, I lose my feet");
+		System.out.println("Now I can shake it, I can shimmy, I can shuffle, I can griddy");
+		System.out.println("(You can griddy?) I can griddy, it just don't look pretty");
+
+
+	}
+
+}
